0e7d509992602a54380f510ac526a0405c18e9fa
Shyam <92642242+shyambera@users.noreply.github.com>
2023-07-27 12:06:42 +0530

    second commit
    new change in router

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -17,6 +17,7 @@ router.route("/order/:id").get(isAuthenticatedUser, getSingleOrder);
 
 router.route("/orders/me").get(isAuthenticatedUser, myOrders);
 
+router.route("/order/new").post(isAuthenticatedUser, newOrder);
 router
   .route("/admin/orders")
   .get(isAuthenticatedUser, authorizeRoles("admin"), getAllOrders);
